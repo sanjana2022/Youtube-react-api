803126137949df910ea68f70b32c5a825ea0af6a
sanjana2022 <76242617+sanjana2022@users.noreply.github.com>
2022-01-18 13:38:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import YTSearch from 'youtube-api-search';
-// Components
+
 import SearchBar from './components/SearchBar/SearchBar';
 import VideoList from './components/VideoList/VideoList';
 import VideoDetail from './components/VideoDetail/VideoDetail';
 import Nav from './components/Nav/Nav';
-// styles
+
 import { Container } from './appStyles.js';
 // Personal Key, it would be hidden for you. Get One!
 import API_KEY from './key';
